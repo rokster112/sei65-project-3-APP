32300ea90feedf8899523a139ef7483144943de3
rokas <rokster112@gmail.com>
2025-04-09 17:47:16 +0300

    topicPage is also throwing errors

--- src/components/TopicPage.js
+++ src/components/TopicPage.js
@@ -1,41 +1,37 @@
-// Import Hooks
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import placeholder from '../styles/images/No-image.png'
 import { API_URL } from '../config'
 
-// Import Bootstrap Components
 import Loading from './Loading'
 
 const TopicPage = () => {
-  const [ topic, setTopic ] = useState([])
-  const [ errors, setErrors] = useState(false)
+  const [topic, setTopic] = useState([]) 
+  const [errors, setErrors] = useState(false)
   const [resStatus, setResStatus] = useState(null)
-  const [data, setData] = useState([])
   const [search, setSearch] = useState('')
   const [filteredTopics, setFilteredTopics] = useState([])
 
-  // ! execution
   const handleChange = (event) => {
     setSearch(event.target.value)
   }
-  
-  useEffect(() => {
-    const regexSearch = new RegExp(search, 'i')
-    const filteredArray = topic.filter(topLine => {
-      return regexSearch.test(topLine.topic) 
-    })
-    setFilteredTopics(filteredArray)
-  }, [search, topic])
-  console.log('search', search)
 
-  // ! Executed
+  useEffect(() => {
+    if (Array.isArray(topic)) {
+      const regexSearch = new RegExp(search, 'i')
+      const filteredArray = topic.filter(topLine => regexSearch.test(topLine.topic))
+      setFilteredTopics(filteredArray)
+    } else {
+      console.log('topic is not an array', topic)
+    }
+  }, [search, topic])
+
   useEffect(() => {
     const getData = async () => {
       try {
         const { data } = await axios.get(`${API_URL}/topic`)
-        setTopic(data)
+        setTopic(Array.isArray(data) ? data : []) 
       } catch (error) {
         console.log(error)
         setErrors(true)
@@ -46,28 +42,28 @@ const TopicPage = () => {
 
   const likeTopic = async (Id, firstLike) => {
     try {
-      
       console.log(localStorage.getItem('userName'))
       const body = { like: firstLike + 1 }
       const res = await axios.put(`${API_URL}/topic${Id}`, body)
       setResStatus(body)
       console.log(res.data.message)
-    } catch (error){
+    } catch (error) {
       console.log(error)
     }
   }
+
   const dislikeTopic = async (Id, firstLike) => {
     try {
-      
       console.log(localStorage.getItem('userName'))
       const body = { dislike: firstLike + 1 }
       const res = await axios.put(`${API_URL}/topic${Id}`, body)
       setResStatus(body)
       console.log(res.data.message)
-    } catch (error){
+    } catch (error) {
       console.log(error)
     }
   }
+
   return (
     <div className="view">
       <div className="topic-div">
@@ -82,15 +78,11 @@ const TopicPage = () => {
       </div>
       <div className="topic-container">
         {filteredTopics.length > 0 
-          ? 
-          filteredTopics.map(titles => {
+          ? filteredTopics.map(titles => {
             const { _id, topic, description, imageUrl, topicUser, createdAt, like, dislike } = titles
-          
-            // console.log('topic', topic)
             const date = createdAt.split('T')[0]
             const time = createdAt.split('T')[1]
             const actualTime = time.split('.')[0]
-            // console.log('time', actualTime)
             return (
               <div key={_id} className="topic">
                 <div className="topic-text">
@@ -100,19 +92,17 @@ const TopicPage = () => {
                     <div className="description">{description}</div>
                   </Link>
                   <div className="topic-like">
-                    <button onClick={() => likeTopic( _id, like )}>👍
-                      <span>{like}</span></button>
-                    <button onClick={() => dislikeTopic( _id, dislike )}>👎<span>{dislike}</span></button>
+                    <button onClick={() => likeTopic(_id, like)}>👍 <span>{like}</span></button>
+                    <button onClick={() => dislikeTopic(_id, dislike)}>👎 <span>{dislike}</span></button>
                   </div> 
                 </div> 
                 <div className="topic-image">
-                  <img className="image" src={imageUrl ? imageUrl : placeholder}></img>
+                  <img className="image" src={imageUrl ? imageUrl : placeholder} alt="Topic"/>
                 </div>
-              </div> 
+              </div>
             )
           })
-          :
-          <>
+          : <>
             {errors ? <h2>Something went wrong. Please try again later</h2> : <Loading />}
           </>
         }
@@ -120,4 +110,5 @@ const TopicPage = () => {
     </div>
   )
 }
+
 export default TopicPage
